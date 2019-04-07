3c87dfa10d73d42ebbbebedbe5045cad8253df67
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-04-07 19:53:13 -0400

    Update Swift 5

--- xcodeTest.sh
+++ xcodeTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd SeptaJSON
-xcodebuild clean build -project SeptaJSON.xcodeproj -scheme SeptaJSON CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone X,OS=12.1' -quiet
-xcodebuild test -project SeptaJSON.xcodeproj -scheme SeptaJSON -destination 'platform=iOS Simulator,name=iPhone X,OS=12.1'  -enableCodeCoverage  YES -quiet
+xcodebuild clean build -project SeptaJSON.xcodeproj -scheme SeptaJSON CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2' -quiet
+xcodebuild test -project SeptaJSON.xcodeproj -scheme SeptaJSON -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2'  -enableCodeCoverage  YES -quiet
